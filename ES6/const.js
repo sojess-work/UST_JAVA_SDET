592f526adb8d19d6482d8a136489e8f6fbdde729
sojess-work <92088687+sojess-work@users.noreply.github.com>
2021-10-19 16:52:40 +0530

    Add files via upload

---
+++ ES6/const.js
@@ -0,0 +1,6 @@
+var x= 10;
+{
+    const x=12;
+    console.log("The value of x inside block is " +x);
+}
+console.log("The value outside block is " +x);
---
+++ ES6/let.js
@@ -0,0 +1,6 @@
+let x=5;
+{
+    let x=10;
+    console.log("The value of x when used inside of the block is "+x);
+}
+console.log("The value of x when used outside of the block is " + x);
